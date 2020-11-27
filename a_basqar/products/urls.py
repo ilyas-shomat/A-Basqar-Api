0745313e096f0d2e8e993d3379e4663b22d73f57
Ilyas Shomat <ilyas.shomat@gmail.com>
2020-11-27 11:23:46 +0600

    Got Company Categories and Products

--- a_basqar/products/urls.py
+++ a_basqar/products/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     ############ Common #############
-    path('common_companies/', views.get_all_common_categories, name="get_all_common_categories"),
+    path('common_categories', views.get_all_common_categories, name="get_all_common_categories"),
+
+    ############ Category #############
+    path('categories', views.get_each_company_categories, name="get_each_company_categories"),
+
+    ############ Products #############
+    path('products/<int:category_id>', views.get_each_company_products_in_selected_category, name="get_each_company_products_in_selected_category"),
 ]
+
